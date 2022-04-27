776b60d59980612079215441b8fce02a97841e9d
Phimlaphat Surayaem <fernphimlaphat@gmail.com>
2022-04-27 17:36:04 +0700

    template ครบ

--- justSaid_project-main/justSaid_project/members/forms.py
+++ justSaid_project-main/justSaid_project/members/forms.py
@@ -20,12 +20,9 @@ class ProfilePageForm(forms.ModelForm):
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','email','password1','password2')
+        fields = ('username','email','password1','password2')
 
 
     def __init__(self, *args, **kwargs):
